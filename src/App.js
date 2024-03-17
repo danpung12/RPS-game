06663cf89174d7044423735ae8541e731f4b7621
aass6863 <aass6863@naver.com>
2024-03-18 01:08:22 +0900

    update

--- src/App.js
+++ src/App.js
@@ -10,15 +10,22 @@ function App() {
 
   const [hand, setHand] = useState('rock');
   const [otherHand, setOtherHand] = useState('rock');
+  const [total, setTotal] = useState([]);
 
   function handleButtonClick(nextHand){
     const nextOtherHand = RandomHand();
+    const nextHistory = getResult(hand, otherHand);
     setHand(nextHand);
     setOtherHand(nextOtherHand);
+    setTotal([...total, nextHistory]);
+  }
+
+  function handleClear(){
+    setTotal([]);
   }
   return (
     <div>
-      <Button> 처음부터 </Button>
+      <Button onClick={handleClear}> 처음부터 </Button>
       <p>{getResult(hand, otherHand)}</p>
       <div>
         <HandIcon value={hand}/>
@@ -26,6 +33,7 @@ function App() {
         <HandIcon value={otherHand}/>
 
       </div>
+      <p>승부 기록: {total.join(', ')}</p>
       <div>
         <HandButton value ="rock" onClick={handleButtonClick}></HandButton>
         <HandButton value ="scissor" onClick={handleButtonClick}></HandButton>
