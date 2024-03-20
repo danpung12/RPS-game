b22cc7b435a9b22f7c4619f52861b7ef5c330f0d
aass6863 <aass6863@naver.com>
2024-03-20 18:54:35 +0900

    pull

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import HandIcon from './HandIcon';
 import { getResult, RandomHand } from './utils';
 import {useState} from 'react';
 
-
+const INITIAL_VALUE = 'rock';
 
 function App() {
 
   const [hand, setHand] = useState('rock');
   const [otherHand, setOtherHand] = useState('rock');
   const [total, setTotal] = useState([]);
+  const [bet, setBet] = useState(0);
+  const [score, setScore] = useState(0);
+  const [otherscore, setOtherScore] = useState(0);
 
   function handleButtonClick(nextHand){
     const nextOtherHand = RandomHand();
@@ -18,20 +21,37 @@ function App() {
     setHand(nextHand);
     setOtherHand(nextOtherHand);
     setTotal([...total, nextHistory]);
+    if (nextHistory=='승리') setScore(score+bet);
+    if (nextHistory=='패배') setOtherScore(otherscore+bet);
   }
 
   function handleClear(){
+    setHand(INITIAL_VALUE);
+    setOtherHand(INITIAL_VALUE);
     setTotal([]);
+  }
+
+  function handleBetChange(e){
+      setBet(Number(e.target.value));
+      
   }
   return (
     <div>
       <Button onClick={handleClear}> 처음부터 </Button>
-      <p>{getResult(hand, otherHand)}</p>
+    
+      <div>
+        {score} : {otherscore}
+      </div>
       <div>
         <HandIcon value={hand}/>
         VS
         <HandIcon value={otherHand}/>
 
+      </div>
+      <div>
+      <input type="number" value={bet} min={1} max={9} onChange={handleBetChange}/>;
+        
+        
       </div>
       <p>승부 기록: {total.join(', ')}</p>
       <div>
